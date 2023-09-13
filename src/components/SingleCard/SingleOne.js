154b259735af6895783227bf5e69fe1f8520fa3d
Kavindi Fernando <it21388248@my.sliit.lk>
2023-09-13 23:23:06 +0530

    edited

--- src/components/SingleCard/SingleOne.js
+++ src/components/SingleCard/SingleOne.js
@@ -15,108 +15,121 @@ const SingleOne = () => {
 
   return (
     <>
-    
-<div>  <div className="containerS">
-     <Header/>
-      
-        <div className="boxS">
-          <div
-            className="upper-partSingle"
-            style={{ backgroundColor: color }}
-          >
+      <div style={{position:"relative"}}>
+        <div className="containerS">
+          <Header />
 
-<div className="arrow-icon" onClick={() => navigate(-1)}>
-              <BsArrowLeftShort className="w-8 h-8" />
-            </div>
+          <div className="boxS">
+            <div
+              className="upper-partSingle"
+              style={{ backgroundColor: color }}
+            >
+              <div className="arrow-icon" onClick={() => navigate(-1)}>
+                <BsArrowLeftShort className="w-8 h-8" />
+              </div>
 
-            
-            <div className="citySingle">
-              {cityData.name}, {cityData.sys.country}
-            </div>
-            <div className="time mt-0">
-              {new Date(cityData.dt * 1000).toLocaleString([], {
-                month: "short",
-                day: "numeric",
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </div>
-            <div className="upper-bottomSingle">
-              <div className="upper-left"></div>
-              <div className="ss">
-                <div className="upper-rightSingle">
-                  <FontAwesomeIcon
-                    className="iconS"
-                    icon={getWeatherIcon(cityData.weather[0].icon)}
-                    size="2x"
-                  />
-                  <div className="descriptionS">
-                    {cityData.weather[0].description}
-                  </div>
-                </div>
-                <div className="upper-leftSingle">
-                  <div className="temperatureSingle">
-                    {Math.round(cityData.main.temp)}°C
-                  </div>
-                  <div className="small-text">
-                    Temp Min: {Math.round(cityData.main.temp_min)}°C
-                  </div>
-                  <div className="small-text">
-                    Temp Max: {Math.round(cityData.main.temp_max)}°C
-                  </div>
-                </div>
+              <div className="citySingle">
+                {cityData.name}, {cityData.sys.country}
               </div>
-            </div>
-          </div>
-          <div className="lower-part">
-            <div className="lower-left ms-0">
-              <div>
-                <p className="bold-text">Pressure:</p> {cityData.main.pressure} hPa
-              </div>
-              <div>
-                <p className="bold-text ms-0">Humidity:</p> {cityData.main.humidity}%
-              </div>
-              <div>
-                <p className="bold-text">Visibility:</p>{" "}
-                {(cityData.visibility / 1000).toFixed(1)} km
-              </div>
-            </div>
-            <div className="lower-center">
-              <div>
-                <TiLocationArrowOutline className="w-8 h-8" />
-              </div>
-              <div>
-                {cityData.wind.speed}m/s {cityData.wind.deg} Degree
-              </div>
-            </div>
-            <div className="lower-right">
-              <div>
-                <p className="bold-text">Sunrise: </p>
-                {new Date(cityData.sys.sunrise * 1000).toLocaleTimeString([], {
+              <div className="time mt-0">
+                {new Date(cityData.dt * 1000).toLocaleString([], {
+                  month: "short",
+                  day: "numeric",
                   hour: "2-digit",
                   minute: "2-digit",
                 })}
               </div>
-              <div>
-                <p className="bold-text">Sunset: </p>
-                {new Date(cityData.sys.sunset * 1000).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
+              <div className="upper-bottomSingle">
+                <div className="upper-left"></div>
+                <div className="ss">
+                  <div className="upper-rightSingle">
+                    <FontAwesomeIcon
+                      className="iconS"
+                      icon={getWeatherIcon(cityData.weather[0].icon)}
+                      size="2x"
+                    />
+                    <div className="descriptionS">
+                      {cityData.weather[0].description}
+                    </div>
+                  </div>
+                  <div className="upper-leftSingle">
+                    <div className="temperatureSingle">
+                      {Math.round(cityData.main.temp)}°C
+                    </div>
+                    <div className="small-text">
+                      Temp Min: {Math.round(cityData.main.temp_min)}°C
+                    </div>
+                    <div className="small-text">
+                      Temp Max: {Math.round(cityData.main.temp_max)}°C
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="lower-part">
+              <div className="lower-left ms-0">
+                <div>
+                  <p className="bold-text">Pressure:</p>{" "}
+                  {cityData.main.pressure} hPa
+                </div>
+                <div>
+                  <p className="bold-text ms-0">Humidity:</p>{" "}
+                  {cityData.main.humidity}%
+                </div>
+                <div>
+                  <p className="bold-text">Visibility:</p>{" "}
+                  {(cityData.visibility / 1000).toFixed(1)} km
+                </div>
+              </div>
+              <div className="lower-center">
+                <div>
+                  <TiLocationArrowOutline className="w-8 h-8" />
+                </div>
+                <div>
+                  {cityData.wind.speed}m/s {cityData.wind.deg} Degree
+                </div>
+              </div>
+              <div className="lower-right">
+                <div>
+                  <p className="bold-text">Sunrise: </p>
+                  {new Date(cityData.sys.sunrise * 1000).toLocaleTimeString(
+                    [],
+                    {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    }
+                  )}
+                </div>
+                <div>
+                  <p className="bold-text">Sunset: </p>
+                  {new Date(cityData.sys.sunset * 1000).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </div>
               </div>
             </div>
           </div>
+
+          <div className="footer w-full mt-8"></div>
         </div>
-       
+        {/* <Footer /> */}
+        <footer
+        className="h-11 bg-gray-800 text-white ml-0"
+        style={{
+          position: "fixed",
+          bottom: 0,
+          justifyContent: "center",
+          backgroundColor: "pink",
+          width: "100%",
+        }}
+      >
+        <p>This is my footer</p>
+      </footer>
+      </div>
 
-     <div className="footer w-full mt-8">
-        <Footer />
-      </div> 
-    </div>
-</div>
-   
-
-</>
+      
+    </>
   );
 };
 
--- src/components/WeatherApp/WeatherCard.js
+++ src/components/WeatherApp/WeatherCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TiLocationArrowOutline } from "react-icons/ti";
 import { RxCross2 } from "react-icons/rx";
@@ -8,7 +8,7 @@ import API_URL from "../../APIHelper";
 import Data from "../../cities.json";
 import Footer from "../footer";
 import Header from "../Header";
-import { getWeatherIcon } from "../../constants";
+import { getWeatherIcon, REFRESH_INTERVAL, METERS_TO_KILOMETERS,MILLISECONDS_TO_SECONDS} from "../../constants";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const API_KEY =
@@ -18,9 +18,9 @@ const WeatherApp = () => {
   // State variables
   const [weatherData, setWeatherData] = useState([]);
   const [selectedCityIndex, setSelectedCityIndex] = useState(null);
+  const [error, setError] = useState(null); // New error state
   const [hiddenBoxes, setHiddenBoxes] = useState([]);
   const navigate = useNavigate();
-
 
   const apiURL = `${API_URL}/group?id=${Data.map((city) => city.CityCode).join(
     ","
@@ -29,7 +29,7 @@ const WeatherApp = () => {
   const fetchWeatherData = async () => {
     try {
       const cachedData = JSON.parse(localStorage.getItem("weatherData"));
-      if (cachedData && Date.now() - cachedData.timestamp < 5 * 60 * 1000) {
+      if (cachedData && Date.now() - cachedData.timestamp < REFRESH_INTERVAL) {
         console.log(Date.now());
         // Use cached data if it's less than 5 minutes old
         setWeatherData(cachedData.data.list);
@@ -37,7 +37,10 @@ const WeatherApp = () => {
         const response = await fetch(apiURL);
 
         if (!response.ok) {
-          throw new Error("Failed to fetch weather data");
+          // throw new Error("Failed to fetch weather data");
+          throw new Error(
+            `Failed to fetch weather data. Status: ${response.status}`
+          );
         }
 
         const data = await response.json();
@@ -51,6 +54,9 @@ const WeatherApp = () => {
     } catch (error) {
       // Handle the error state
       console.error(error);
+      setError(
+        "An error occurred while fetching weather data. Please try again later."
+      );
     }
   };
 
@@ -65,11 +71,10 @@ const WeatherApp = () => {
     setHiddenBoxes((prevHiddenBoxes) => [...prevHiddenBoxes, index]);
   };
 
- 
   useEffect(() => {
     // Remove duplicate API requests by checking cached data
     const cachedData = JSON.parse(localStorage.getItem("weatherData"));
-    if (cachedData && Date.now() - cachedData.timestamp < 5 * 60 * 1000) {
+    if (cachedData && Date.now() - cachedData.timestamp < REFRESH_INTERVAL) {
       setWeatherData(cachedData.data.list);
     } else {
       fetchWeatherData();
@@ -85,143 +90,137 @@ const WeatherApp = () => {
   if (Data.length === 0 || weatherData.length === 0) {
     return (
       <div className="flex justify-center items-center h-screen">
-        Loading...
+        {error || "Loading..."}
       </div>
     );
   }
 
   return (
-    <>  
-    <div>
-      <Header />
-      <div className="AddCity flex justify-center pb-5">
-        <input
-          type="text"
-          className=" h-8 bg-gray-800 rounded-l-sm p-2 text-white"
-          id="myInput"
-          placeholder="Enter a city"
-        />
-        <input
-          type="button"
-          className=" bg-purple-700 b-2 w-24 h-8 rounded-md text-white font-bold"
-          value="Add City"
-          onClick={submitForm}
-        />
-      </div>
+    <>
+      <div>
+        <Header />
+        <div className="AddCity flex justify-center pb-5">
+          <input
+            type="text"
+            className=" h-8 bg-gray-800 rounded-l-sm p-2 text-white"
+            id="myInput"
+            placeholder="Enter a city"
+          />
+          <input
+            type="button"
+            className=" bg-purple-700 b-2 w-24 h-8 rounded-md text-white font-bold"
+            value="Add City"
+          />
+        </div>
 
-      <div className="box-container  ">
-        {weatherData.map(
-          (cityData, index) =>
-            // Check if the box should be hidden
-            !hiddenBoxes.includes(index) && (
-              <div
-                className="box "
-                key={index}
-                onClick={() => handleCityBoxClick(index, cityData)}
-              >
-                {/* Upper Part */}
-                <div className="weather-box ">
-                  <div
-                    className="upper-part"
-                    style={{ backgroundColor: predefinedColors[index] }}
-                  >
-                    <div className="cross">
-                      <RxCross2
-                        className="w-5 h-5"
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          handleRemoveBoxClick(index);
-                        }}
-                      />
-                    </div>
-                    <div className="upper-left">
-                      <div className="city">
-                        {Data[index].CityName}, {cityData.sys.country}
-                      </div>
-                      <div className="time mt-0 text-sm">
-                        {new Date(cityData.dt * 1000).toLocaleString([], {
-                          month: "short",
-                          day: "numeric",
-                          hour: "2-digit",
-                          minute: "2-digit",
-                        })}
-                      </div>
-                      <div className="description ml-2">
-                        <FontAwesomeIcon
-                          className="weather-iconM mr-2 mt-3"
-                          icon={getWeatherIcon(cityData.weather[0].icon)}
+        <div className="box-container  ">
+          {weatherData.map(
+            (cityData, index) =>
+              // Check if the box should be hidden
+              !hiddenBoxes.includes(index) && (
+                <div
+                  className="box "
+                  key={index}
+                  onClick={() => handleCityBoxClick(index, cityData)}
+                >
+                  {/* Upper Part */}
+                  <div className="weather-box ">
+                    <div
+                      className="upper-part"
+                      style={{ backgroundColor: predefinedColors[index] }}
+                    >
+                      <div className="cross">
+                        <RxCross2
+                          className="w-5 h-5"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleRemoveBoxClick(index);
+                          }}
                         />
-                        {cityData.weather[0].description}
                       </div>
-                    </div>
-                    <div className="upper-right">
-                      <div className="temperature">
-                        {Math.round(cityData.main.temp)}°C
+                      <div className="upper-left">
+                        <div className="city">
+                          {Data[index].CityName}, {cityData.sys.country}
+                        </div>
+                        <div className="time mt-0 text-sm">
+                          {new Date(cityData.dt * 1000).toLocaleString([], {
+                            month: "short",
+                            day: "numeric",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          })}
+                        </div>
+                        <div className="description ml-2">
+                          <FontAwesomeIcon
+                            className="weather-iconM mr-2 mt-3"
+                            icon={getWeatherIcon(cityData.weather[0].icon)}
+                          />
+                          {cityData.weather[0].description}
+                        </div>
                       </div>
-                      <div>
-                        Temp Min: {Math.round(cityData.main.temp_min)}°C
-                      </div>
-                      <div>
-                        Temp Max: {Math.round(cityData.main.temp_max)}°C
+                      <div className="upper-right">
+                        <div className="temperature">
+                          {Math.round(cityData.main.temp)}°C
+                        </div>
+                        <div>
+                          Temp Min: {Math.round(cityData.main.temp_min)}°C
+                        </div>
+                        <div>
+                          Temp Max: {Math.round(cityData.main.temp_max)}°C
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                {/* Lower Part */}
-                <div className="lower-part">
-                  <div className="lower-left ms-0">
-                    <div>
-                      <p className="bold-text">Pressure:</p>{" "}
-                      {cityData.main.pressure}hPa
+                  {/* Lower Part */}
+                  <div className="lower-part">
+                    <div className="lower-left ms-0">
+                      <div>
+                        <p className="bold-text">Pressure:</p>{" "}
+                        {cityData.main.pressure}hPa
+                      </div>
+                      <div>
+                        <p className="bold-text ms-0">Humidity:</p>
+                        {cityData.main.humidity}%
+                      </div>
+                      <div>
+                        <p className="bold-text">Visibility:</p>{" "}
+                        {(cityData.visibility *  METERS_TO_KILOMETERS).toFixed(1)} km
+                      </div>
                     </div>
-                    <div>
-                      <p className="bold-text ms-0">Humidity:</p>
-                      {cityData.main.humidity}%
-                    </div>
-                    <div>
-                      <p className="bold-text">Visibility:</p>{" "}
-                      {(cityData.visibility / 1000).toFixed(1)} km
-                    </div>
-                  </div>
 
-                  <div className="lower-center">
-                    <div>
-                      <TiLocationArrowOutline className="w-8 h-8" />
+                    <div className="lower-center">
+                      <div>
+                        <TiLocationArrowOutline className="w-8 h-8" />
+                      </div>
+                      <div>
+                        {cityData.wind.speed}m/s {cityData.wind.deg} Degree
+                      </div>
                     </div>
-                    <div>
-                      {cityData.wind.speed}m/s {cityData.wind.deg} Degree
-                    </div>
-                  </div>
-                  <div className="lower-right">
-                    <div>
-                      <p className="bold-text">Sunrise: </p>
-                      {new Date(
-                        cityData.sys.sunrise * 1000
-                      ).toLocaleTimeString()}
-                    </div>
-                    <div>
-                      <p className="bold-text">Sunset: </p>
-                      {new Date(
-                        cityData.sys.sunset * 1000
-                      ).toLocaleTimeString()}
+                    <div className="lower-right">
+                      <div>
+                        <p className="bold-text">Sunrise: </p>
+                        {new Date(
+                          cityData.sys.sunrise * MILLISECONDS_TO_SECONDS
+                        ).toLocaleTimeString()}
+                      </div>
+                      <div>
+                        <p className="bold-text">Sunset: </p>
+                        {new Date(
+                          cityData.sys.sunset * MILLISECONDS_TO_SECONDS
+                        ).toLocaleTimeString()}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            )
-        )}
+              )
+          )}
+        </div>
       </div>
-      
-    </div>
-    <div className=" mt-10">
-    <Footer />
-  </div>
-   </>
+      <div className=" mt-10">
+        <Footer />
+      </div>
+    </>
   );
 };
-
-function submitForm() {
-  //form submission logic,if needed
-}
 
 export default WeatherApp;
